ad25f405cd94dc641ef1084384398c9e9688c357
victorgarciaz <victor.garcia@bootcamp-upgrade.com>
2023-01-22 15:57:09 +0100

    feat: install bcrypt, mongo, dotenv, express-session y passport-local

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express"); 
+const passport = require("passport");
+const session = require("express-session");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MongoStore = require("connect-mongo");
 const cors = require("cors"); 
 const db = require("./src/utils/db");
 
 db.connectDB(); 
 
 const indexRoutes = require("./src/api/index/index.routes");
-//const usersRoutes = require("./src/api/users/user.routes");
 const authorsRoutes = require("./src/api/authors/author.routes");
-//const listenersRoutes = require("./src/api/listeners/listener.routes");
 const songsRoutes = require ("./src/api/songs/music.routes");
+//const usersRoutes = require("./src/api/users/user.routes");
 
 
 const PORT = 3000;
@@ -21,12 +26,22 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.use("/", indexRoutes);
-//server.use("/users", usersRoutes);
-server.use("/authors", authorsRoutes);
-//server.use("/listeners", listenersRoutes);
-server.use("/songs", songsRoutes);
+server.use(passport.initialize());
+server.use(passport.session());
+server.use(
+  session({
+      secret: process.env.SESSION_SECRET,
+      saveUninitialized: true,
+      resave: false,
+      cookie: { maxAge: 5 * 60 * 1000 },
+      store: MongoStore.create({ mongoUrl: db.DB_URL }),
+  })
+);
 
+server.use("/", indexRoutes);
+server.use("/authors", authorsRoutes);
+server.use("/songs", songsRoutes);
+//server.use("/users", usersRoutes);
 
 server.use("*", (req, res, next) => {
   return res.status(404).json("No se encuentra la URL. Prueba con otra URL");
